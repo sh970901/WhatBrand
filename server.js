2b154bbe2c3712b99ee82c3de8532860d91100a2
sh970901 <aaddss639@naver.com>
2022-03-11 16:49:48 +0900

    server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(cors())
 
+
 const connection = mysql.createConnection({
     host: conf.host,
     user: conf.user,
