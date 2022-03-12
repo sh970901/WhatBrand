cb54c984b13d2561f69691cca39b842c11338418
sh970901 <aaddss639@naver.com>
2022-03-13 04:27:55 +0900

    local로 재설정

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 const cors = require('cors')
 const mysql = require('mysql');
 const fs = require('fs');
